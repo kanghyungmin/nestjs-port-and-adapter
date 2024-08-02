7d3bfb2528be5dbd792139ccfd060c81666d99bd
hyungmin <hmkkang0922@daum.net>
2024-08-02 12:24:48 +0900

    init commit

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+// // import { NestFactory } from '@nestjs/core';
+// // import { AppModule } from './app.module';
+
+// // async function bootstrap() {
+// //   const app = await NestFactory.create(AppModule);
+// //   await app.listen(3000);
+// // }
+// // bootstrap();
+// import { ServerApplication } from '@application/ServerApplication';
+
+// async function runApplication() {
+//   const serverApplication : ServerApplication = ServerApplication.new();
+//   await serverApplication.start();
+// }
