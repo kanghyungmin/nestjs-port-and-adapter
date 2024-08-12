db1a3e761d8eb23ca022a44c074584a376369dd6
hyungmin <hmkkang0922@daum.net>
2024-08-12 12:21:35 +0900

    add utility assert statement

---
+++ src/core/common/util/assert/CoreAssert.ts
@@ -0,0 +1,68 @@
+
+import { Nullable, Optional } from "@core/common/type/CommonType";
+
+export class CoreAssert {
+    public static isDefined<T>(value : T, exception: Error) : void {
+        if (value === undefined || value === null) {
+            throw exception;
+        }
+    }
+    public static isString(value : any, exception: Error) : void {
+        if (typeof value !== 'string') {
+            throw exception;
+        }
+    }
+    public static isNumber(value : any, exception: Error) : void {
+        if (typeof value !== 'number') {
+            throw exception;
+        }
+    }
+    public static isBoolean(value : any, exception: Error) : void {
+        if (typeof value !== 'boolean') {
+            throw exception;
+
+        }
+    }
+    public static isArray(value : any, exception: Error) : void {
+        if (!Array.isArray(value)) {
+            throw exception;
+
+        }
+    }
+    public static isObject(value : any, exception: Error) : void {
+        if (typeof value !== 'object') {
+            throw exception;
+
+        }
+    }
+    public static isFunction(value : any, exception: Error) : void {
+        if (typeof value !== 'function') {
+            throw exception;
+
+        }
+    }
+    public static isInstanceOf(value : any, clazz : any, exception: Error) : void {
+        if (!(value instanceof clazz)) {
+            throw exception;
+
+        }
+    }
+    public static isTrue(value : boolean, exception: Error) : void {
+        if (value !== true) {
+            throw exception;
+
+        }
+    }
+    public static isFalse(value : boolean, exception: Error) : void {
+        if (value !== false) {
+            throw exception;
+
+        }
+    }
+    public static isNotEmpty(value : any, exception: Error) : void {
+        if (value === undefined || value === null || value === '') {
+            throw exception;
+        }
+    }
+}
+
