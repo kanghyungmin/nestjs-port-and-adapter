fc9400897a31cb36492be176e5a95a8c74d712e2
hyungmin <hmkkang0922@daum.net>
2024-08-13 13:18:27 +0900

    add user reps's port

---
+++ src/core/domain/user/port/persistence/UserRepositoryPort.ts
@@ -0,0 +1,10 @@
+import { User } from '@core/domain/user/entity/User';
+import { Optional } from '@core/common/type/CommonType';
+
+
+export interface UserRepositoryPort {
+    findUser(by: {id?: string, email?: string}) : Promise<Optional<User>>;
+    countUsers(by:{id?:string, email?: string}) : Promise<number>;
+    addUser(user:User) : Promise<void>;
+    updateUser(user: User) : Promise<void>;
+}
