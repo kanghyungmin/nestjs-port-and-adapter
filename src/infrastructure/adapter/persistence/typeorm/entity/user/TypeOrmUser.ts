83ba1304c0956f23a997798b87deed2185be8327
hyungmin <hmkkang0922@daum.net>
2024-08-19 17:12:43 +0900

    add User Domain & ORM

---
+++ src/infrastructure/adapter/persistence/typeorm/entity/user/TypeOrmUser.ts
@@ -0,0 +1,30 @@
+import { Column, Entity, PrimaryColumn } from "typeorm";
+
+
+@Entity({ name: 'users' })
+export class TypeOrmUser {
+    @PrimaryColumn()
+    public id: string;
+
+    @Column() 
+    public firstName: string;
+
+    @Column() 
+    public lastName : string
+
+    @Column() 
+    public  email : string
+
+    @Column() 
+    public password: string;
+
+    @Column() 
+    public createdAt : Date;
+
+    @Column() 
+    public updatedAt : Date;
+
+    @Column() 
+    public removedAt : Date;
+
+}
---
+++ src/infrastructure/adapter/persistence/typeorm/entity/user/mapper/TypeOrmUserMapper.ts
@@ -0,0 +1,45 @@
+import { User } from "@core/domain/user/entity/User";
+import { TypeOrmUser } from "../TypeOrmUser";
+
+
+export class TypeOrmUserMapper {
+    public static toOrmEntity(domainUser : User) : TypeOrmUser {
+        const ormUser = new TypeOrmUser();
+        
+        ormUser.id = domainUser.getId();
+        ormUser.firstName = domainUser.getFirstName();
+        ormUser.lastName = domainUser.getLastName();
+        ormUser.email = domainUser.getEmail();
+        ormUser.password = domainUser.getPassword();
+
+        ormUser.createdAt = domainUser.getCreatedAt();
+        ormUser.updatedAt = domainUser.getUpdatedAt() as Date;
+        ormUser.removedAt = domainUser.getRemovedAt() as Date;
+
+        return ormUser;
+    }
+
+    public static toOrmEntities(domainUsers : User[]) : TypeOrmUser[] {
+        return domainUsers.map( domainUser => this.toOrmEntity(domainUser))
+    }
+
+    public static toDomainEntity(ormUser : TypeOrmUser) : User {
+        const domainUser: User = new User({
+
+            id: ormUser.id,
+            firstName: ormUser.firstName,
+            lastName: ormUser.lastName,
+            email: ormUser.email,
+            password: ormUser.password,
+            createdAt: ormUser.createdAt,
+            updatedAt: ormUser.updatedAt,
+            removedAt: ormUser.removedAt
+        })
+        return domainUser
+    }
+    
+    public static toDomainEntities(ormUsers: TypeOrmUser[]) : User[] {
+        return ormUsers.map( ormUser => this.toDomainEntity(ormUser))
+    }
+
+}
