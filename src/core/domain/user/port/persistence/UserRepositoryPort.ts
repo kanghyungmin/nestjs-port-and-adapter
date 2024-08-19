4ccd0d0262b796f9868573e82a11662583079d2b
hyungmin <hmkkang0922@daum.net>
2024-08-19 17:11:57 +0900

    cng user repo interface

--- src/core/domain/user/port/persistence/UserRepositoryPort.ts
+++ src/core/domain/user/port/persistence/UserRepositoryPort.ts
@@ -5,6 +5,6 @@ import { Optional } from '@core/common/type/CommonType';
 export interface UserRepositoryPort {
     findUser(by: {id?: string, email?: string}) : Promise<Optional<User>>;
     countUsers(by:{id?:string, email?: string}) : Promise<number>;
-    addUser(user:User) : Promise<void>;
+    addUser(user: User): Promise<{id: string}>;
     updateUser(user: User) : Promise<void>;
 }
