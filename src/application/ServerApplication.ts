12b6ed37c0d6b4fd367b02815373e8bafd6cb9be
hyungmin <hmkkang0922@daum.net>
2024-08-21 13:58:07 +0900

    cng boot strapping logic

--- src/application/ServerApplication.ts
+++ src/application/ServerApplication.ts
@@ -1,15 +1,15 @@
 import { ApiServerConfig } from "@infrastructure/config/ApiServerConfig";
 import { Logger } from "@nestjs/common";
 import { NestApplication, NestFactory } from "@nestjs/core";
-import { NestExpressApplication } from "@nestjs/platform-express";
 import { DocumentBuilder, OpenAPIObject, SwaggerModule } from "@nestjs/swagger";
+import { RootModule } from './di/RootModule'
 
 export class ServerApplication {
     private readonly host : string = ApiServerConfig.HOST;
     private readonly port : number = ApiServerConfig.PORT;
 
     public async run() : Promise<void> {
-        const app : NestExpressApplication = await NestFactory.create<NestApplication>(RootModule)
+        const app : NestApplication = await NestFactory.create<NestApplication>(RootModule)
         this.buildAPIDocumentation(app)
         this.logs()
         await app.listen(this.port)
@@ -18,20 +18,21 @@ export class ServerApplication {
         Logger.log(`Server running on host : ${this.host}, port : ${this.port}`, ServerApplication.name)
     }
     
-    private buildAPIDocumentation(app: NestExpressApplication) : void{
+    private buildAPIDocumentation(app: NestApplication) : void{
         const title : string = "Nestjs-DDD-Templates";
         const description : string = "Nestjs-DDD-Templates API Documentation";
         const version : string = "1.0.0";
 
         const options : Omit<OpenAPIObject, 'paths'>  = new DocumentBuilder()
-        .setTitle(title)
-        .setDescription(description)
-        .setVersion(version)
+        .setVersion('3.0.0') // Add the version property
+        .setTitle(title) // Set the title
+        .setDescription(description) // Set the description
+        .setVersion(version) // Set the version
         .addBearerAuth({
             type: 'apiKey',
             in: 'header',
             name: ApiServerConfig.ACCESS_TOKEN_HEADER
-        })
+        }).build()
 
         const document : OpenAPIObject = SwaggerModule.createDocument(app, options);
         SwaggerModule.setup('docs', app, document);
