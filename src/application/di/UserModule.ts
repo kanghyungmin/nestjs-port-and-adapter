e168efc9d2279ef344b665bb10874edfdea54a62
hyungmin <hmkkang0922@daum.net>
2024-08-30 17:59:28 +0900

    enroll FindUserService on Provider

--- src/application/di/UserModule.ts
+++ src/application/di/UserModule.ts
@@ -2,12 +2,11 @@ import { UserController } from "@application/controller/UserController"
 import { CoreDITokens } from "@core/common/di/CoreDITokens"
 import { UserDITokens } from "@core/domain/user/di/UserDITokens"
 import { CreateUserService } from "@core/service/user/CreateUserService"
+import { FindUserService } from "@core/service/user/FindUserService"
 import { HandleGetUserPreviewQueryService } from "@core/service/user/HandlerGetUserPreviewQueryService"
-import { TypeOrmUser } from "@infrastructure/adapter/persistence/typeorm/entity/user/TypeOrmUser"
 import { TypeOrmUserRepositoryAdapter } from "@infrastructure/adapter/persistence/typeorm/repository/user/TypeOrmUserRepositoryAdapter"
 import { NestWrapperGetUserPreviewQueryHandler } from "@infrastructure/handler/user/NestWrapperGetUserPreviewQueryHandler"
 import { Module, Provider } from "@nestjs/common"
-import { TypeOrmModule } from "@nestjs/typeorm"
 import { DataSource } from "typeorm"
 
 
@@ -26,16 +25,20 @@ const persistenceProviders: Provider[] = [
     {
       provide   : UserDITokens.CreateUserUseCase,
       useFactory: (userRepository) => new CreateUserService(userRepository),
-      inject    : [UserDITokens.UserRepository]
+      inject    : [UserDITokens.UserRepository, CoreDITokens.QueryBus]
     },
+    {
+      provide   : UserDITokens.FindUserUseCase,
+      useClass : FindUserService
+    },
+    
   ]
   
   const handlerProviders: Provider[] = [
     NestWrapperGetUserPreviewQueryHandler,
     {
       provide   : UserDITokens.GetUserPreviewQueryHandler,
-      useFactory: (userRepository) => new HandleGetUserPreviewQueryService(userRepository),
-      inject    : [UserDITokens.UserRepository]
+      useClass: HandleGetUserPreviewQueryService,
     }
   ]
   
--- test.ts
+++ test.ts
@@ -1,53 +1,7 @@
-function isSafe(board: string[], row: number, col: number): boolean {
-    for(let i=0; i< row; i++){
-        if(board[i][col]=='Q') return false;
-    }
-    for(let i=0; i< col; i++){
-        if(board[row][i]=='Q') return false;
-    }
-
-    for(let i= row-1, j=col-1; i>=0&& j>=0; i--,j--)
-        if(board[i][j]=='Q') return false;
-
-    for(let i= row-1, j=col+1; i>=0 && j>=0; i--,j++)
-        if(board[i][j]=='Q') return false;
-
-
-
-    return true
+function climbStairs(n: number): number {
+    if(n==1)
+        return 1
+    else 
+        return climbStairs(n-1) + 1
     
-}
-function solveNQueens(n: number): string[][] {
-    const results : string[][] = [];
-    const board : string[] = Array(n).fill('.'.repeat(n))
-
-    function solve(row :number) {
-        if(row == n) {
-            results.push([...board])
-            return;
-        }
-        for(let col=0; col < n; col++) {
-            if(isSafe(board,row,col)) {
-                board[row] = board[row].substring(0,col) +'Q' +board[row].substring(col+1)           
-                solve(row+1)
-                board[row] = board[row].substring(0,col) +'.' +board[row].substring(col+1)
-            }
-
-        }
-    }
-    solve(0)
-    return results
-    
-}
-
-
-
-
-
-const n = 4;
-const solutions = solveNQueens(n);
-console.log(`Number of solutions for ${n}-Queens: ${solutions.length}`);
-solutions.forEach((solution, index) => {
-    console.log(`Solution ${index + 1}:`);
-    solution.forEach(row => console.log(row));
-});
+};
