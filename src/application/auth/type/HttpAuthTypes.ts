4aa2a5dbad9eda9db03d1bb0f079cb495537158f
hyungmin <hmkkang0922@daum.net>
2024-09-02 13:57:47 +0900

    add auth types

---
+++ src/application/auth/type/HttpAuthTypes.ts
@@ -0,0 +1,17 @@
+import { Request } from 'express'
+
+export type HttpUserPayload = {
+  id: string,
+  email: string,
+};
+
+export type HttpRequestWithUser = Request & {user: HttpUserPayload};
+
+export type HttpJwtPayload = {
+  id: string,
+};
+
+export type HttpLoggedInUser = {
+  id: string,
+  accessToken: string,
+};
---
+++ src/application/controller/documentation/auth/HttpRestApiResponseLoggedInUser.ts
@@ -0,0 +1,11 @@
+
+import { ApiProperty } from '@nestjs/swagger'
+import { HttpRestApiResponse } from '../common/HttpRestApiResponse';
+import { HttpRestApiModelLoggedInUser } from './HttpRestApiModelLoggedInUser';
+
+export class HttpRestApiResponseLoggedInUser extends HttpRestApiResponse {
+  
+  @ApiProperty({type: HttpRestApiModelLoggedInUser})
+  public data: HttpRestApiResponseLoggedInUser;
+  
+}
