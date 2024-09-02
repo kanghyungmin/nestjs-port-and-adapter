804f9102a1c920c450d89ae15ed82b0812897304
hyungmin <hmkkang0922@daum.net>
2024-09-02 16:17:35 +0900

    chore : cng controller to test jwt-auth

--- src/application/controller/UserController.ts
+++ src/application/controller/UserController.ts
@@ -3,14 +3,16 @@ import { UserDITokens } from "@core/domain/user/di/UserDITokens";
 import { CreateUserUseCase } from "@core/domain/user/usecase/CreateUserUseCase";
 import { UserUseCaseDto } from "@core/domain/user/usecase/dto/UserUseCaseDto";
 import { CreateUserAdapter } from "@infrastructure/adapter/usecase/CreateUserAdapter";
-import { Body, Controller, Get, HttpCode, HttpStatus, Inject, Post, Query } from "@nestjs/common";
-import { ApiBody,  ApiResponse, ApiTags } from "@nestjs/swagger";
+import { Body, Controller, Get, HttpCode, HttpStatus, Inject, Post, Query, Req, UseGuards } from "@nestjs/common";
+import { ApiBearerAuth, ApiBody,  ApiResponse, ApiTags } from "@nestjs/swagger";
 import { HttpRestApiModelCreateUserBody } from "@application/controller/documentation/user/HttpRestApiModelCreateUserBody";
 import { HttpRestApiResponseUser } from "@application/controller/documentation/user/HttpRestApiResponseUser";
 import { HttpRestApiModelFindUserQuery } from "./documentation/user/HttpRestApiModelFindUserQuery";
 import { FindUserAdapter } from "@infrastructure/adapter/usecase/FindUserAdapter";
 import { Optional } from "@core/common/type/CommonType";
 import { FindUserService } from "@core/service/user/FindUserService";
+import { HttpJwtAuthGuard } from "@application/auth/guard/HttpJwtAuthGuard";
+import { HttpRequestWithUser } from "@application/auth/type/HttpAuthTypes";
 
 @Controller('user')
 @ApiTags('user')
@@ -40,15 +42,17 @@ export class UserController {
       return CoreApiResponse.success(createdUser)
     }
     @Get('account')
+    @UseGuards(HttpJwtAuthGuard)
     @HttpCode(HttpStatus.OK)
+    @ApiBearerAuth()
     @ApiResponse({ status: HttpStatus.OK, type: HttpRestApiResponseUser})
-    public async findAccound(@Query() query : HttpRestApiModelFindUserQuery) : Promise<CoreApiResponse<Optional<UserUseCaseDto>>>  {
+    public async findAccound(@Req() request: HttpRequestWithUser,@Query() query : HttpRestApiModelFindUserQuery) : Promise<CoreApiResponse<Optional<UserUseCaseDto>>>  {
+      console.log(typeof(request.user.id))
       const adapter : FindUserAdapter = await FindUserAdapter.new({
         id : query.id
       })
+      // console.log(query)
       const findedUser : Optional<UserUseCaseDto> = await this.findUserUseCase.execute(adapter)
       return CoreApiResponse.success(findedUser)
-
     }
-
 }
